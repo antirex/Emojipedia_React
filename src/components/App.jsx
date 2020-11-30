4f83d066c252f006725c3c2909c2e61c7c661078
antirex <55479324+antirex@users.noreply.github.com>
2020-11-30 12:49:41 +0530

    counter_using_useSate_hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,43 @@
-import React from "react";
-import emojipedia from "../emojipedia";
-import Heading from "./Heading";
-import Emoji from "./Emoji";
-
-function createEmoji(emojipedia) {
-  return (
-    <Emoji
-      key={emojipedia.id}
-      emoji_img={emojipedia.emoji}
-      name={emojipedia.name}
-      desc={emojipedia.meaning}
-    />
-  );
-}
+import React, {useState} from "react";
+// import emojipedia from "../emojipedia";
+// import Heading from "./Heading";
+// import Emoji from "./Emoji";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function incrementCount() {
+    setCount(count+1);
+  }
+  function decrementCount(){
+    setCount(count-1);
+  }
   return (
-    <div>
-      <Heading />
-      {emojipedia.map(createEmoji)}
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>+</button>
+      <button onClick={decrementCount}>-</button>
     </div>
   );
 }
+
+// function createEmoji(emojipedia) {
+//   return (
+//     <Emoji
+//       key={emojipedia.id}
+//       emoji_img={emojipedia.emoji}
+//       name={emojipedia.name}
+//       desc={emojipedia.meaning}
+//     />
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Heading />
+//       {emojipedia.map(createEmoji)}
+//     </div>
+//   );
+// }
 
 export default App;
